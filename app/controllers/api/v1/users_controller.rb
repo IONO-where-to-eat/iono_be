f20bc57e7d769af9acbbf4c3c60a08e921f48aa3
Thomas Hayes <hayes.thomas610@gmail.com>
2022-11-03 16:27:23 -0600

    add controller, db, serializer, and spec for users to send to FE.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    binding.pry
     user = User.find_or_create_by(email: user_params[:email])
     user.update(user_params)
     render json: UserSerializer.new(user)
