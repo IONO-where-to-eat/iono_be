d3e90445c626bfdc3f56f3ee5c2688968b5429c6
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-11-03 11:14:56 -0600

    add serializer and create method for user. change port in puma to 5000.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.new()
+    user = User.find_or_create_by(email: user_params[:email])
+    user.update(user_params)
+    render json: UserSerializer.new(user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :email, :token, :google_id)
   end
 end
