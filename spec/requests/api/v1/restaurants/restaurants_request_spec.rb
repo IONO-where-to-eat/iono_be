8b506f37262c6133d3e5f45e2c4ded2f142f2476
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-11-08 15:59:52 -0700

    update yelp service to return 50 restaurants

--- spec/requests/api/v1/restaurants/restaurants_request_spec.rb
+++ spec/requests/api/v1/restaurants/restaurants_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Restaurant API request from FE' do
   it 'sends list of restaurants' do
-    
+
     allow(LocationFacade).to receive(:location).and_return([39.37221145629883, -104.85608673095703])
     get api_v1_restaurants_path
 
@@ -11,6 +11,19 @@ RSpec.describe 'Restaurant API request from FE' do
     expect(response).to be_successful
     expect(restaurants).to be_a Hash
     expect(restaurants[:businesses]).to be_an Array
-    expect(restaurants[:businesses].count).to eq(20)
+    expect(restaurants[:businesses].count).to eq(50)
+  end
+
+  it 'sends list of restaurants with params' do
+    allow(LocationFacade).to receive(:location).and_return([39.37221145629883, -104.85608673095703])
+    filter_params = {:radius=>5, :open_now=>"true", :price=>"3"}
+
+    get api_v1_restaurants_path(filter_params)
+
+    restaurants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(restaurants).to be_a Hash
+    expect(restaurants[:businesses]).to be_an Array
   end
 end
