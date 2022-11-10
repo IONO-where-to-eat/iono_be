7490bd50be04ad7085f3ac713cc7f8ab09040278
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-11-10 06:48:32 -0700

    minor cleanup/refactor

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,11 +1,9 @@
 class LocationService
   def self.get_location
-    response = self.conn.get('/v1/geocode/ip')
+    response = conn.get('/v1/geocode/ip')
     parse(response)
   end
 
-  private
-  
   def self.conn
     Faraday.new(url: 'https://api.radar.io') do |faraday|
       faraday.headers['Authorization'] = ENV['location_api_key']
@@ -15,4 +13,6 @@ class LocationService
   def self.parse(response)
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  private_class_method :conn, :parse
 end
